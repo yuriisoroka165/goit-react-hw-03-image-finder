cd8705f4986113dd8c5b9f0259778382b868b7c3
Yurii Soroka <yurisoroka165@gmail.com>
2023-03-02 13:38:02 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+// import css from "./App.module.css";
+// import Modal from "./Modal";
+
+class App extends Component {
+    state = {};
+
+    // componentDidMount() {
+    //     const contacts = localStorage.getItem("contacts");
+    //     const parsedContacts = JSON.parse(contacts);
+    //     if (parsedContacts) {
+    //         this.setState({ contacts: parsedContacts });
+    //     }
+    // };
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     if (this.state.contacts !== prevState.contacts) {
+    //         localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+    //     }
+    // };
+
+    render() {
+        return <></>;
+    }
+}
+
+export default App;
