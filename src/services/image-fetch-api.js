a38ba4acb79a7b991c5392955c840208f778733f
Yurii Soroka <yurisoroka165@gmail.com>
2023-03-06 21:51:22 +0200

    updates

--- src/services/image-fetch-api.js
+++ src/services/image-fetch-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const fetchImages = async (query, page, key = API_KEY) => {
     const { data } = await axios.get(
