cf06651c9d918dc90bcb992bcd2aad95a2585eba
Yurii Soroka <yurisoroka165@gmail.com>
2023-03-06 20:05:00 +0200

    key

--- src/components/services/image-fetch-api.js
+++ src/components/services/image-fetch-api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
+import { API_KEY } from "./api-key";
 
-const PRIVATE_KEY = "33457552-f72b8f2d874a669f815eb264f";
-
-const fetchImages = async (query, page, key = PRIVATE_KEY) => {
+const fetchImages = async (query, page, key = API_KEY) => {
     const { data } = await axios.get(
         `https://pixabay.com/api/?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`,
         { withCredentials: false }
